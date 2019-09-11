8f350718ceeb77697fda59a66b9679cab63dae20
Melissa <47627411+MelissaAlasalmi@users.noreply.github.com>
2019-09-11 23:14:31 +0300

    Add files via upload

---
+++ rev_print.c
@@ -0,0 +1,24 @@
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i;
+	i = 0;
+
+	if (argc != 2)
+		write(1, "\n", 1);
+	else
+	{
+		while (argv[1][i])
+			i++;
+		i--;
+		while (i >=0)
+		{
+			write(1, &argv[1][i], 1);
+			i--;
+		}
+	write(1, "\n", 1);
+	}
+	return (0);
+}
